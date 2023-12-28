eab76e77baab14e4c8e4283ad3a8279d0c1a38b5
Yusuf Sönmez <Yusufsonmez951@gmail.com>
2023-12-28 13:04:16 +0300

    Day2 BirthdayProblem

---
+++ ProblemSolutions/Day2 BirthdayProblem/BirthdayProblem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions.Day2_BirthdayProblem
+{
+    public class BirthdayProblem
+    {
+        private const int DAYS = 365;
+
+        public double calculateProbility(int number)
+        {
+            double probability = 1;
+            for(var i = 0;i<number;i++)
+            {
+                probability *= ((double)(DAYS - i) / DAYS);
+            }
+
+            return 1 - probability;
+        }
+
+
+    }
+}
--- TestProblemSolutions/Program.cs
+++ TestProblemSolutions/Program.cs
@@ -1,4 +1,5 @@
 ﻿using ProblemSolutions.Day1_PascalTriangle;
+using ProblemSolutions.Day2_BirthdayProblem;
 
 namespace TestProblemSolutions
 {
@@ -9,6 +10,12 @@ namespace TestProblemSolutions
             /* Day 1 Pascal Triangle Test
             PascalTriangle pascalTriangle = new PascalTriangle(12);
             pascalTriangle.PascalTriangleWrite();
+            */
+
+            /* Day 2 BirthdayProblem Test
+            in a random group of 23 people, there is about a 50 percent chance that two people have the same birthday.
+            BirthdayProblem birthdayProblem = new BirthdayProblem();
+            Console.WriteLine(birthdayProblem.calculateProbility(23));
             */
         }
     }
