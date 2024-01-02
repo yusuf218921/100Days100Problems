58e2248a4e26980998aaef22cfc030e402777a7e
Yusuf Sönmez <Yusufsonmez951@gmail.com>
2024-01-02 13:06:59 +0300

    PiNumber Calculator

---
+++ ProblemSolutions/Day7 PiNumber/PiNumberCalculator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProblemSolutions.Day7_PiNumber
+{
+    public class PiNumberCalculator
+    { 
+        public double Run(ulong n)
+        {
+            double result = 0;
+            for(ulong i = 0; i <= n; i++)
+            {
+                result += (double)Math.Pow(-1, i) / (2 * i + 1);
+            }
+            double pi = (double)result * 4;
+            Console.WriteLine(pi);
+            return pi;
+        }
+    }
+}
--- TestProblemSolutions/Program.cs
+++ TestProblemSolutions/Program.cs
@@ -4,6 +4,7 @@ using ProblemSolutions.Day3_CollatzConjecture;
 using ProblemSolutions.Day4_FindDublicate;
 using ProblemSolutions.Day5_PolindromString;
 using ProblemSolutions.Day6_EulerNumber;
+using ProblemSolutions.Day7_PiNumber;
 using System.Numerics;
 
 namespace TestProblemSolutions
@@ -20,14 +21,21 @@ namespace TestProblemSolutions
             */
 
 
+            /* Day 2 Birthday Problem
+            
+            BirthdayProblem birthdayProblem = new BirthdayProblem();
+            Console.WriteLine(birthdayProblem.calculateProbility(23));
+            
+            */
 
-            //BirthdayProblem birthdayProblem = new BirthdayProblem();
-            //Console.WriteLine(birthdayProblem.calculateProbility(23));
 
 
-
-            //CollatzConjectures collatzConjectures = new CollatzConjectures();
-            //collatzConjectures.Run(35132132183271834567634532m);
+            /* Day 3 Collatz Conjectures
+            
+            CollatzConjectures collatzConjectures = new CollatzConjectures();
+            collatzConjectures.Run(35132132183271834567634532m);
+            
+            */
 
 
 
@@ -45,12 +53,25 @@ namespace TestProblemSolutions
 
             */
 
-            /* Day 6 EulerNumberCalculator */
+            /* Day 6 EulerNumberCalculator
 
-            //EulerCalculator eulerCalculator = new EulerCalculator();
-            //eulerCalculator.RunWithLimit(10000);
-            //eulerCalculator.RunWithOutLimit(99999);
-            //Console.WriteLine("Euler Number -> {0}" , Math.E);
+            EulerCalculator eulerCalculator = new EulerCalculator();
+            eulerCalculator.RunWithLimit(10000);
+            eulerCalculator.RunWithOutLimit(99999);
+            Console.WriteLine("Euler Number -> {0}" , Math.E);
+
+            */
+
+            /* Day 7 PiNumber Calculator 
+            
+            pi = 4 * ((-1)^n) / (2n+1)   n=0 n -> infinity
+            pi = 3,141592653589793
+
+            PiNumberCalculator calculator = new PiNumberCalculator();
+            calculator.Run(10000000000)
+
+            */
+
         }
     }
 }
